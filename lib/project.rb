29783e00701948ebfab8e2a71c5b3ea3cf7e8241
Drew Henderson <drewhendersonemail@gmail.com>
2021-12-26 18:42:04 -0800

    passed second and third test for project

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,15 @@
+
+
 class Project
   attr_accessor :id, :name
 
   def initialize(attributes)
     @id = attributes[:id]
     @name = attributes[:name]
+  end
+  
+  def ==(compared)
+    self.name == compared.name
   end
 
 end
