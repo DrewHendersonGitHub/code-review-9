fcb57decf3a2d469a6345ca0b6d61307d57382b7
Drew Henderson <drewhendersonemail@gmail.com>
2021-12-26 19:00:20 -0800

    passed fourth test for project

--- lib/project.rb
+++ lib/project.rb
@@ -12,4 +12,14 @@ class Project
     self.name == compared.name
   end
 
+  def self.all
+    projects = []
+    returns = DB.exec("SELECT * FROM projects;")
+    returns.each do |project|
+      projects.push(Project.new({name: project.fetch("name"), id: project.fetch("id").to_i}))
+    end
+    projects
+  end
+
+  
 end
