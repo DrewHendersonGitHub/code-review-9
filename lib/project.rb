7701da272cbbbb3491deb1893148bb04c2eb62b2
Drew Henderson <drewhendersonemail@gmail.com>
2021-12-26 19:11:32 -0800

    passed eighth test for project

--- lib/project.rb
+++ lib/project.rb
@@ -25,5 +25,14 @@ class Project
     result = DB.exec("INSERT INTO projects (name) VALUES ('#{@name}') RETURNING id;")
     @id = result.first().fetch("id").to_i
   end
-  
+
+  def self.find(id)
+    project = DB.exec("SELECT * FROM projects WHERE id = #{id};").first
+    if project
+      Project.new({name: project.fetch("name"), id: project.fetch("id").to_i})
+    end
+  end
+
+
+
 end
