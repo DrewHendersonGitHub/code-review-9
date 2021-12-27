eb354fc948e61934233eda3682e72e6a6972ea36
Drew Henderson <drewhendersonemail@gmail.com>
2021-12-26 21:11:53 -0800

    passed first integration spec

--- app.rb
+++ app.rb
@@ -1,11 +1,27 @@
 require 'sinatra'
 require 'sinatra/reloader'
 also_reload 'lib/**/*.rb'
+require './lib/project'
+require './lib/volunteer'
 require 'pry'
 require "pg"
 
-#DB = PG.connect({ dbname: 'volunteer_tracker_test', host: 'db', user: 'postgres', password: 'password' })
+DB = PG.connect({ dbname: 'volunteer_tracker', host: 'db', user: 'postgres', password: 'password' })
 
 get '/' do
-  "This is connected to the database ."
+  @projects = Project.all
+  erb(:projects)
 end
+
+get '/projects' do
+  @projects = Project.all
+  erb(:projects)
+end
+
+post '/projects' do
+  project = Project.new({name: params[:name]})
+  project.save
+  @projects = Project.all
+  erb(:projects)
+end
+
